09d15bffa070b4c1b04613475144d657f89896aa
Thanh Phan <thanh.phan@altsourcesoftware.com>
2023-02-08 13:49:24 +0700

    update code

--- src/Screens/HomeBoard.js
+++ src/Screens/HomeBoard.js
@@ -20,7 +20,6 @@ const HomeBoard = ({ navigation }) => {
   const [data, setData] = useState(null);
   useEffect(() => {
     getListProduct();
-    alert("aaaa");
   }, []);
   const getListProduct = () => {
     const apiURL = "http://103.116.107.230:8081/api/product/product/branch";
@@ -172,312 +171,43 @@ const HomeBoard = ({ navigation }) => {
                 }}
               >
                 {Object.values(product)[0] &&
-                  Object.values(product)[0].slice(0,4).map((productD) => (
-                    <Pressable
-                      onPress={() => navigation.navigate("Single", productD)}
-                      key={productD.id}
-                      w="47%"
-                      bg={Colors.white}
-                      rounded="md"
-                      shadow={2}
-                      pt={0.3}
-                      my={3}
-                      pb={2}
-                      overflow="hidden"
-                    >
-                      <Image
-                        source={{
-                          uri: `http://192.168.1.50:8083/image/Asset/${productD.image}`,
-                        }}
-                        alt={productD.productName}
-                        w="full"
-                        h={24}
-                        resizeMode="contain"
-                      />
-                      <Box px={4} pt={1}>
-                        <Heading size="sm" bold>
-                          đ{productD.newPrice}
-                        </Heading>
-                        <Text fontSize={15} mt={1} isTruncated w="full">
-                          {productD.productName}
-                        </Text>
-                      </Box>
-                    </Pressable>
-                  ))}
+                  Object.values(product)[0]
+                    .slice(0, 4)
+                    .map((productD) => (
+                      <Pressable
+                        onPress={() => navigation.navigate("Single", productD)}
+                        key={productD.id}
+                        w="47%"
+                        bg={Colors.white}
+                        rounded="md"
+                        shadow={2}
+                        pt={0.3}
+                        my={3}
+                        pb={2}
+                        overflow="hidden"
+                      >
+                        <Image
+                          source={{
+                            uri: `http://103.116.107.230:8081/image/Asset/${productD.image}`,
+                          }}
+                          alt={productD.productName}
+                          w="full"
+                          h={24}
+                          resizeMode="contain"
+                        />
+                        <Box px={4} pt={1}>
+                          <Heading size="sm" bold>
+                            đ{productD.newPrice}
+                          </Heading>
+                          <Text fontSize={15} mt={1} isTruncated w="full">
+                            {productD.productName}
+                          </Text>
+                        </Box>
+                      </Pressable>
+                    ))}
               </View>
             </View>
           ))}
-
-        {/* <View
-          style={{
-            padding: 16,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontWeight: "500",
-                  letterSpacing: 1,
-                }}
-              >
-                SAMSUNG
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  color: Colors.black,
-                  fontWeight: "400",
-                  opacity: 0.5,
-                  marginLeft: 10,
-                }}
-              >
-                78
-              </Text>
-            </View>
-            <Text
-              style={{
-                fontSize: 14,
-                color: Colors.blue,
-                fontWeight: "400",
-              }}
-              onPress={()=> navigation.navigate('Home')}
-            >
-              Xem thêm
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              flexWrap: "wrap",
-              justifyContent: "space-around",
-            }}
-          >
-           {products.slice(0,4).map((product) => (
-              <Pressable
-                onPress={() => navigation.navigate("Single", product)}
-                key={product.id}
-                w="47%"
-                bg={Colors.white}
-                rounded="md"
-                shadow={2}
-                pt={0.3}
-                my={3}
-                pb={2}
-                overflow="hidden"
-              >
-                <Image
-                  source={{ uri: product.productImage }}
-                  alt={product.productName}
-                  w="full"
-                  h={24}
-                  resizeMode="contain"
-                />
-                <Box px={4} pt={1}>
-                  <Heading size="sm" bold>
-                    đ{product.productPrice}
-                  </Heading>
-                  <Text fontSize={15} mt={1} isTruncated w="full">
-                    {product.productName}
-                  </Text>
-                </Box>
-              </Pressable>
-            ))}
-          </View>
-        </View>
-        <View
-          style={{
-            padding: 16,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontWeight: "500",
-                  letterSpacing: 1,
-                }}
-              >
-                OPPO
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  color: Colors.black,
-                  fontWeight: "400",
-                  opacity: 0.5,
-                  marginLeft: 10,
-                }}
-              >
-                78
-              </Text>
-            </View>
-            <Text
-              style={{
-                fontSize: 14,
-                color: Colors.blue,
-                fontWeight: "400",
-              }}
-              onPress={()=> navigation.navigate('Home')}
-            >
-              Xem thêm
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              flexWrap: "wrap",
-              justifyContent: "space-around",
-            }}
-          >
-           {products.slice(0,4).map((product) => (
-              <Pressable
-                onPress={() => navigation.navigate("Single", product)}
-                key={product.id}
-                w="47%"
-                bg={Colors.white}
-                rounded="md"
-                shadow={2}
-                pt={0.3}
-                my={3}
-                pb={2}
-                overflow="hidden"
-              >
-                <Image
-                  source={{ uri: product.productImage }}
-                  alt={product.productName}
-                  w="full"
-                  h={24}
-                  resizeMode="contain"
-                />
-                <Box px={4} pt={1}>
-                  <Heading size="sm" bold>
-                    đ{product.productPrice}
-                  </Heading>
-                  <Text fontSize={15} mt={1} isTruncated w="full">
-                    {product.productName}
-                  </Text>
-                </Box>
-              </Pressable>
-            ))}
-          </View>
-        </View>
-        <View
-          style={{
-            padding: 16,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontWeight: "500",
-                  letterSpacing: 1,
-                }}
-              >
-                XIAOMI
-              </Text>
-              <Text
-                style={{
-                  fontSize: 14,
-                  color: Colors.black,
-                  fontWeight: "400",
-                  opacity: 0.5,
-                  marginLeft: 10,
-                }}
-              >
-                78
-              </Text>
-            </View>
-            <Text
-              style={{
-                fontSize: 14,
-                color: Colors.blue,
-                fontWeight: "400",
-              }}
-              onPress={()=> navigation.navigate('Home')}
-            >
-              Xem thêm
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              flexWrap: "wrap",
-              justifyContent: "space-around",
-            }}
-          >
-           {products.slice(0,4).map((product) => (
-              <Pressable
-                onPress={() => navigation.navigate("Single", product)}
-                key={product.id}
-                w="47%"
-                bg={Colors.white}
-                rounded="md"
-                shadow={2}
-                pt={0.3}
-                my={3}
-                pb={2}
-                overflow="hidden"
-              >
-                <Image
-                  source={{ uri: product.productImage }}
-                  alt={product.productName}
-                  w="full"
-                  h={24}
-                  resizeMode="contain"
-                />
-                <Box px={4} pt={1}>
-                  <Heading size="sm" bold>
-                    đ{product.productPrice}
-                  </Heading>
-                  <Text fontSize={15} mt={1} isTruncated w="full">
-                    {product.productName}
-                  </Text>
-                </Box>
-              </Pressable>
-            ))}
-          </View>
-        </View> */}
       </ScrollView>
     </View>
   );
